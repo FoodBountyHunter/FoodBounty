96726562d0625e190b55099a06b0b7fb0e2fbe69
Jason Lehmann <jasonlehmann@Jasons-MacBook-Pro.local>
2015-07-22 16:55:25 -0400

    Established Draft with Implementing a Review creation
    Needs Review Table on Parse

--- FoodBounty/ReviewSubmitViewController.swift
+++ FoodBounty/ReviewSubmitViewController.swift
@@ -4,16 +4,24 @@
 //
 //  Created by Jason Lehmann on 7/22/15.
 //  Copyright (c) 2015 gvsu.edu.team_c. All rights reserved.
+//                          [String,String  , NSDate?  , NSDate,  , float  , float   ,char[140]
+// Assumine Table "Review : [JobID ,HunterID, orderDate, orderTime,qualRate, timeRate, comments]
+//
 //
 
 import UIKit
 import Parse
 import ParseUI
 
-class ReviewSubmitViewController: UIViewController {
+class ReviewSubmitViewController: UIViewController ,UITextViewDelegate{
           override func viewDidLoad() {
         super.viewDidLoad()
+            CommentTextField.delegate = self
+            
             let predicate = NSPredicate(format: "OrderID = testOrderID")
+            
+            
+            
             
                           // Do any additional setup after loading the view.
     }
@@ -37,14 +45,18 @@ class ReviewSubmitViewController: UIViewController {
     override func viewDidAppear(animated: Bool) {
         dispatch_async(dispatch_get_main_queue(), {
             var query   = PFQuery(className: "Review")
-            
-            query.whereKey("OrderID", equalTo:"testOrderIDValue ")
+            //equal Is a placeholder
+            query.whereKey("JobID", equalTo:"testOrderIDValue ")
             query.findObjectsInBackgroundWithBlock {
                 (objects: [AnyObject]?, error: NSError?) -> Void in
                 
                 if error == nil {
                     // The find succeeded.
-                    //  self.OrderNumber.title = objects["OrderID"] as! String?
+                    /*
+Should enable
+
+
+*/
                    self.OrderDayLabel.text =  Optional(objects![0]["orderDate"] as! String)
                    self.OrderTimeLabel.text = Optional(objects![0]["orderTime"] as! String)
                    self.BountyHunterName.text = Optional(objects![0]["HunterID"] as! String)
@@ -62,13 +74,15 @@ class ReviewSubmitViewController: UIViewController {
     }
 
     
-    
+    // Should send to Profile View Controller or the Review List view controller
     @IBAction func CancelReview(sender: UIBarButtonItem) {
         
         //change to Profile View Contorller
          dismissViewControllerAnimated(true, completion: nil)
     }
     
+    
+    //Saves action and sends to Profile
     @IBAction func SubmitReview(sender: UIBarButtonItem) {
         
         var ff: Float = 0.5
@@ -79,7 +93,7 @@ class ReviewSubmitViewController: UIViewController {
         newRow["Order"] = false
         newRow["Reviewer"] = nil */
         
-        newRow["QualRat"] = QualitySlideVal.value % 0.5
+        newRow["qualRate"] = QualitySlideVal.value % 0.5
        newRow["timeRate"] = timelinessSliderVal.value % 0.5
        newRow["comments"] = CommentTextField.text
       //  newRow["complete"]
@@ -91,11 +105,24 @@ class ReviewSubmitViewController: UIViewController {
 
     }
 
+    @IBOutlet weak var timelinessSliderValue: UILabel!
     @IBAction func timelinessSlider(sender: UISlider) {
         
-    }
-    @IBAction func QualitySlider(sender: UISlider) {
+        timelinessSliderValue.text = ( timelinessSliderVal.value % 0.5).description
         
     }
-
+    @IBOutlet weak var QualityLabel: UILabel!
+    @IBAction func QualitySlider(sender: UISlider) {
+         QualityLabel.text =  (QualitySlideVal.value % 0.5).description
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        var maxtext = 140
+        //If the text is larger than the maxtext, the return is false
+        return count(textView.text) + (count(text) - range.length) <= maxtext
+        
+    }
+    
+    
+    
 }
